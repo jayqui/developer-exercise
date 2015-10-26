7460a547c3ccad1ea59f2ff54b7fce9a8f066085
Jay Quigley <quigley.james.g@gmail.com>
2015-10-26 18:17:54 -0500

    introduce apparently functional sliding hide/show behavior

--- javascript/accordion/custom.js
+++ javascript/accordion/custom.js
@@ -1,8 +1,17 @@
 $(function () {
+	// initial setup: show first
 	$(".accordion-header div").hide();
+	$(".accordion-header:first-of-type").addClass("active").children("div").show();
+
+	// 
 	$(".accordion-header").on("click",function(event) {
 		event.preventDefault();
-	$(".accordion-header div").hide();
-		$(this).children("div").show();
+		if (!$(this).hasClass("active")) {
+			$(".accordion-header").removeClass("active");
+			$(".accordion-header div").slideUp();
+
+			$(this).addClass("active").children("div").slideToggle();
+		};
 	});
+
 });
