5697e5217df07b122067230da8904dfa6336eeaa
Jay Quigley <quigley.james.g@gmail.com>
2015-10-27 11:38:23 -0500

    add initial solution to YouTube challenge

---
+++ ruby/blackjack/card_symbols.rb
@@ -0,0 +1,9 @@
+spade = "\u2660"
+club = "\u2663"
+heart = "\u2665"
+diamond = "\u2666"
+
+puts spade.encode('utf-8')
+puts club.encode('utf-8')
+puts heart.encode('utf-8')
+puts diamond.encode('utf-8')
---
+++ ruby/youtube/scrape_search.rb
@@ -0,0 +1,35 @@
+###############################
+###############################
+# ENTER YOUR SEARCH QUERY HERE:
+search_query = "cool stuff"
+# HOW MANY VIDEOS WOULD YOU LIKE? (UP TO 20)
+how_many = 3
+###############################
+###############################
+
+
+plusified = search_query.split(" ").join("+")
+
+require 'net/http'
+require 'nokogiri'
+require 'open-uri'
+
+uri = "https://www.youtube.com/results?search_query=#{plusified}"
+doc = Nokogiri::HTML(open(uri))
+h3_objects = doc.css(".yt-lockup-title").take(how_many)
+results = h3_objects.map do |h3|
+	links = h3.children.select {|child| child.name == "a"}
+	links.map do |link|
+		"https://www.youtube.com" + link.attributes["href"].value
+	end[0]
+end
+p results
+
+
+# Note to self: for future coolness:
+
+#### Obviously, you could add a front end that supplies the string and number via html <input>s
+#
+#### You could also embed the results right on the search page with the 'embed' feature (see tab below any video)---they just give you an iframe like so:
+#
+# <iframe width="560" height="315" src="https://www.youtube.com/embed/Cmuc2Zu7I0Q" frameborder="0" allowfullscreen></iframe>
