6de55291a33b8631cf8658715087b231c7ea8638
Jay Quigley <quigley.james.g@gmail.com>
2015-10-30 13:58:55 -0500

    change Hand to take splat operator; add #score method

--- ruby/blackjack/models/hand.rb
+++ ruby/blackjack/models/hand.rb
@@ -1,7 +1,20 @@
 class Hand
-  attr_accessor :cards
+	attr_reader :player
+  attr_accessor :cards, :score
 
-  def initialize
-    @cards = []
+  def initialize(player, *cards)
+    @cards = cards
+    @player = player
   end
+
+  def score
+  	@cards.inject(0) do |accum, card|
+  		if card.value.is_a?(Fixnum)
+  			accum + card.value
+  		elsif card.value.is_a?(Array)
+  			accum + card.value[1]
+  		end
+  	end
+  end
+
 end
