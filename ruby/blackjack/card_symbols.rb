eb4c4975ac63602b7f1ac0ba6e34f3cc6d45f290
Jay Quigley <quigley.james.g@gmail.com>
2015-10-30 11:51:56 -0500

    write spec tests for Deck and Card

--- ruby/blackjack/card_symbols.rb
+++
@@ -1,9 +0,0 @@
-spade = "\u2660"
-club = "\u2663"
-heart = "\u2665"
-diamond = "\u2666"
-
-puts spade.encode('utf-8')
-puts club.encode('utf-8')
-puts heart.encode('utf-8')
-puts diamond.encode('utf-8')
