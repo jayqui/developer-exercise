7968edc84a8dfbcb4e4c27a91bf350cb1c590c4d
Jay Quigley <quigley.james.g@gmail.com>
2015-10-31 11:34:20 -0500

    method to set players

--- ruby/blackjack/controllers/game.rb
+++ ruby/blackjack/controllers/game.rb
@@ -1,15 +1,16 @@
+Dir[File.dirname(__FILE__) + "/../models/*.rb"].each {|file| require file}
 require_relative "../views/game/cli"
 
 class GameController
-	attr_reader :view
+	attr_reader :view, :round
 
 	def initialize
 		@view = GameView.new
-		# @round = Round.new
 	end
 
 	def set_players
-		view.introductions
+		players = view.introduction
+		@round = Round.new(players)
 	end
 
 
