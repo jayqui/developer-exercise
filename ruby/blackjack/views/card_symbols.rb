0696a014ad39d35a9c66852c995e5c13d5fd919b
Jay Quigley <quigley.james.g@gmail.com>
2015-10-31 11:33:57 -0500

    add initial view logic

--- ruby/blackjack/views/card_symbols.rb
+++ ruby/blackjack/views/card_symbols.rb
@@ -7,3 +7,12 @@ puts spade.encode('utf-8')
 puts club.encode('utf-8')
 puts heart.encode('utf-8')
 puts diamond.encode('utf-8')
+
+SYM = {
+	"spade": "\u2660".encode('utf-8'),
+	"club": "\u2663".encode('utf-8'),
+	"heart": "\u2665".encode('utf-8'),
+	"diamond": "\u2666".encode('utf-8')
+}
+	
+# to color red: "\e[31m#{self}\e[0m"
--- ruby/blackjack/views/game/cli.rb
+++ ruby/blackjack/views/game/cli.rb
@@ -6,8 +6,13 @@ class GameView
 
 	def ask_for_player_names
 		puts "Enter the names of up to four players, then press `Enter`"
-		text = ""
-		
+		text, counter, names = "", 0, []
+		while text != ""
+			text = gets.chomp
+			names << text
+			counter += 1
+		end
+		names
 	end
 
 end
