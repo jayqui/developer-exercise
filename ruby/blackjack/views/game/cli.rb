11c8a0198e3ad9e2d3b3edd9746f54fecca47233
Jay Quigley <quigley.james.g@gmail.com>
2015-10-30 20:16:34 -0500

    add beginnings of command line interface

---
+++ ruby/blackjack/views/game/cli.rb
@@ -0,0 +1,13 @@
+class GameView
+
+	def introduction
+		puts "Welcome to Blackjack\n\n"
+	end
+
+	def ask_for_player_names
+		puts "Enter the names of up to four players, then press `Enter`"
+		text = ""
+		
+	end
+
+end
