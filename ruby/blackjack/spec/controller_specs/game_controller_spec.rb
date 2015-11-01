df66e9062b834e60dd04201a86fabeeda3cc3e48
Jay Quigley <quigley.james.g@gmail.com>
2015-11-01 12:30:30 -0600

    in controller spec, change before block to before(:each) block & add pending spec for #play_game

--- ruby/blackjack/spec/controller_specs/game_controller_spec.rb
+++ ruby/blackjack/spec/controller_specs/game_controller_spec.rb
@@ -5,25 +5,26 @@ describe "GameController" do
 	let(:gc) { GameController.new }
 	let(:view) { GameView.new }
 	
-	describe "set_players" do
-		before do
-      io_obj = double
-      expect(view)
-        .to receive(:gets)
-        .and_return(io_obj)
-        .at_least(:once)
-      expect(io_obj)
-        .to receive(:chomp)
-        .and_return("Cal")
-      expect(io_obj)
-        .to receive(:chomp)
-        .and_return("Dale")
-      expect(io_obj)
-        .to receive(:chomp)
-        .and_return("")
+	before(:each) do
+    io_obj = double
+    expect(view)
+      .to receive(:gets)
+      .and_return(io_obj)
+      .at_least(:once)
+    expect(io_obj)
+      .to receive(:chomp)
+      .and_return("Cal")
+    expect(io_obj)
+      .to receive(:chomp)
+      .and_return("Dale")
+    expect(io_obj)
+      .to receive(:chomp)
+      .and_return("")
 
-      allow(gc).to receive(:view) { view }
-    end
+    allow(gc).to receive(:view) { view }
+  end
+
+	describe "#set_players" do
 
 		it "should return a valid round" do
 			expect(gc.set_players).to be_a(Round)
@@ -31,6 +32,13 @@ describe "GameController" do
 
 		it "should have players equivalent to those entered in the view" do
 			expect(gc.set_players.hands.map {|hand| hand.player.name }).to eq(["Cal","Dale","Dealer"])
+		end
+	end
+
+	xdescribe "#play_game" do
+		it "does stuff" do
+			gc.set_players
+			expect(gc.play_game).to eq('hi')
 		end
 	end
 end
