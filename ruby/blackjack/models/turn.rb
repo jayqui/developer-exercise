b4ee33bb6c25a743bc2756b1772930aa697428a0
Jay Quigley <quigley.james.g@gmail.com>
2015-11-01 13:13:12 -0600

    reorganize Turn class

--- ruby/blackjack/models/turn.rb
+++ ruby/blackjack/models/turn.rb
@@ -7,12 +7,14 @@ class Turn
 		@player_stands = false
 	end
 
-	def turn_over?
-		player_stands || hand.is_blackjack || hand.is_busted
-	end
-
 	def hit
 		
+	end
+
+	def double_down
+	end
+
+	def split
 	end
 
 end
