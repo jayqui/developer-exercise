bcbd5ee0e7928b8404cf3f55ce254cf64d2b54c5
Jay Quigley <quigley.james.g@gmail.com>
2015-11-01 21:03:46 -0600

    add stubbed tests for gameplay for both blackjack & non-blackjack hands

--- ruby/blackjack/models/deck.rb
+++ ruby/blackjack/models/deck.rb
@@ -23,16 +23,32 @@ class Deck
   end
 
   def deal_card
-    random = rand(@playable_cards.size)
-    @playable_cards.delete_at(random)
+    random = rand(playable_cards.size)
+    playable_cards.delete_at(random)
   end
 
   def shuffle
     @playable_cards = []
     SUITS.each do |suit|
       NAME_VALUES.each do |name, value|
-        @playable_cards << Card.new(suit, name, value)
+        playable_cards << Card.new(suit, name, value)
       end
     end
   end
+
+  def deal_ace
+    ace = playable_cards.select {|card| card.name == :ace}.sample
+    playable_cards.delete(ace)
+  end
+
+  def deal_non_ace
+    non_ace = playable_cards.reject {|card| card.name == :ace}.sample
+    playable_cards.delete(non_ace)
+  end
+
+  def deal_10_value_card
+    ten_val = playable_cards.select {|card| card.value == 10}.sample
+    playable_cards.delete(ten_val)
+  end
+
 end
--- ruby/blackjack/spec/controller_specs/game_controller_spec.rb
+++ ruby/blackjack/spec/controller_specs/game_controller_spec.rb
@@ -36,13 +36,22 @@ describe "GameController" do
 	end
 
 	describe "#handle_player_actions" do
+
 		context "when hand is not a blackjack" do
 			before(:each) do
+				gc.set_players
+
 				allow(view)
 				.to receive(:ask_for_action)
 				.and_return('h')
 
-				gc.set_players
+				# rig a hand that does not get dealt a blackjack
+				non_lucky_player = Player.new("Joe McPlain")
+				non_lucky_round = Round.new([non_lucky_player])
+				non_lucky_hand_array = [Hand.new(non_lucky_player, gc.round.deck.deal_non_ace, gc.round.deck.deal_non_ace)]
+				allow(non_lucky_round).to receive(:hands) { non_lucky_hand_array }
+				allow(gc).to receive(:round) { non_lucky_round }
+
 				hand = gc.round.hands[0]
 				turn = Turn.new(hand, gc.round)
 				gc.handle_player_actions(turn)
@@ -51,12 +60,44 @@ describe "GameController" do
 			it "should increase the card count by one one for each hit" do
 				expect(gc.round.hands[0].cards.count).to be > 2
 			end
-			it "should hit until a bust" do
-				expect(gc.round.hands[0].is_busted).to be true
-			end
+
+			# 50.times do
+				it "should hit until a bust (every time)" do
+					expect(gc.round.hands[0].is_busted).to be true
+				end
+			# end
+
 		end
+		
 		context "when hand is a blackjack" do
-			
+			before(:each) do
+				gc.set_players
+
+				allow(view)
+				.to receive(:ask_for_action)
+				.and_return('h')
+
+				# rig a lucky hand that gets dealt a (random) blackjack
+				lucky_player = Player.new("Lady Luck")
+				lucky_round = Round.new([lucky_player])
+				lucky_hand_array = [Hand.new(lucky_player, gc.round.deck.deal_ace, gc.round.deck.deal_10_value_card)]
+				allow(lucky_round).to receive(:hands) { lucky_hand_array }
+				allow(gc).to receive(:round) { lucky_round }
+
+				hand = gc.round.hands[0]
+				turn = Turn.new(hand, gc.round)
+				gc.handle_player_actions(turn)
+			end
+
+			# 10.times do
+				it "this mock successfully produces a blackjack in the first hand (always)" do
+					expect(gc.round.hands[0].score == [11,21] && gc.round.hands[0].cards.count == 2).to eq(true)
+				end
+			# end
+
+			it "should return a success message before any action takes place" do
+				expect(gc.round.hands[0].cards.count).to eq(2)
+			end
 		end
 	end
 
