bfcb4e18512a4bd5968fcb6a25bd71cbf67b5c48
Jay Quigley <quigley.james.g@gmail.com>
2015-11-01 19:30:59 -0600

    add a couple new spec methods for controller & view

--- ruby/blackjack/spec/controller_specs/game_controller_spec.rb
+++ ruby/blackjack/spec/controller_specs/game_controller_spec.rb
@@ -35,10 +35,35 @@ describe "GameController" do
 		end
 	end
 
-	xdescribe "#play_game" do
-		it "does stuff" do
-			gc.set_players
-			expect(gc.play_game).to eq('hi')
+	describe "#handle_player_actions" do
+		context "when hand is not a blackjack" do
+			before(:each) do
+				allow(view)
+				.to receive(:ask_for_action)
+				.and_return('h')
+
+				gc.set_players
+				hand = gc.round.hands[0]
+				turn = Turn.new(hand, gc.round)
+				gc.handle_player_actions(turn)
+			end
+
+			it "should increase the card count by one one for each hit" do
+				expect(gc.round.hands[0].cards.count).to be > 2
+			end
+			it "should hit until a bust" do
+				expect(gc.round.hands[0].is_busted).to be true
+			end
+		end
+		context "when hand is a blackjack" do
+			
 		end
 	end
+
+	# xdescribe "#play_game" do
+	# 	it "does stuff" do
+	# 		gc.set_players
+	# 		expect(gc.play_game).to eq('hi')
+	# 	end
+	# end
 end
--- ruby/blackjack/spec/view_specs/cli_spec.rb
+++ ruby/blackjack/spec/view_specs/cli_spec.rb
@@ -27,4 +27,16 @@ describe "GameView" do
     end
 	end
 
+  describe "#ask_for_action" do
+    before do
+      io_obj = double
+      expect(view).to receive(:gets).and_return(io_obj).once
+      expect(io_obj).to receive(:chomp).and_return("h")
+    end
+
+    it "#should return what is inputted" do
+      expect(view.ask_for_action).to eq("h")
+    end
+  end
+
 end
