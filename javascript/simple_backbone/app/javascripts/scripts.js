f00b690640c2dd78778f6d8b3a6b9321d137030e
Jay Quigley <quigley.james.g@gmail.com>
2015-11-04 16:25:40 -0600

    add click event handler with event.preventDefault

--- javascript/simple_backbone/app/javascripts/scripts.js
+++ javascript/simple_backbone/app/javascripts/scripts.js
@@ -26,6 +26,12 @@ var QuoteView = Backbone.View.extend({
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+	events: {
+	  'click .theme-link' : 'sortByTheme'
+	},
+	sortByTheme: function(event) {
+		event.preventDefault();
 	}
 });
 
