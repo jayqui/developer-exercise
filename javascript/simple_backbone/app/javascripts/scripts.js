2154ac002cbaf5bcb7b38fb776beef3da8604c57
Jay Quigley <quigley.james.g@gmail.com>
2015-11-05 00:48:05 -0600

    add apparently functional re-rendering of view on all button click and dropdown (re-)select scenarios (behaviorally tested for now)

--- javascript/simple_backbone/app/javascripts/scripts.js
+++ javascript/simple_backbone/app/javascripts/scripts.js
@@ -1,22 +1,49 @@
 $(document).ready(function() {
+
+	var currentView = quotesView;
+
+	ajaxRequest.done(function() {
+		toggleButtonClasses(currentView);
+	})
+
 	$(".next-button").on("click", function() {
-		quotesView.incrementCounter()
-		quotesView.render("preserveOrder");
+		currentView.incrementCounter()
+		currentView.render("preserveOrder");
+		toggleButtonClasses(currentView);
 	});
+
 	$(".previous-button").on("click", function() {
-		quotesView.decrementCounter()
-		quotesView.render("preserveOrder");
+		currentView.decrementCounter()
+		currentView.render("preserveOrder");
+		toggleButtonClasses(currentView);
 	});
+
 	$(".theme-dropdown").change(function() {
 		var query = $('option:selected').text();
 		if (query === "all") {
-			quotesView.render();
+			currentView = quotesView;
+			currentView.render();
 		} 
 		else {
 			var queryTheme = quotesCollection.where({theme: query});
 			var themedCollection = new QuotesCollection(queryTheme);
-			themedQuotesView = new QuotesView({model: themedCollection});
-			themedQuotesView.render("preserveOrder");
+			currentView = new QuotesView({model: themedCollection});
+			currentView.render("preserveOrder");
 		}
 	})
 });
+
+function toggleButtonClasses(currentView) {
+	if (currentView.counter === 0) {
+		$(".previous-button").addClass("disabled");
+	}
+	else {
+		$(".previous-button").removeClass("disabled");
+	}
+	if (currentView.counter + currentView.incrementBy > currentView.model.length-1) {
+		$(".next-button").addClass("disabled"); 
+	}
+	else {
+		$(".next-button").removeClass("disabled");
+	}
+}
--- javascript/simple_backbone/app/javascripts/views/quotes.js
+++ javascript/simple_backbone/app/javascripts/views/quotes.js
@@ -1,6 +1,7 @@
 // View for all quotes
 var QuotesView = Backbone.View.extend({
 	counter: 0,
+	incrementBy: 3,
 	model: quotesCollection,
 	el: $('.quotes-list'),
 	initialize: function() {
@@ -15,7 +16,7 @@ var QuotesView = Backbone.View.extend({
 		var self = this;
 		this.$el.html('');
 		var toArray = this.model.toArray();
-		var nextThree = toArray.slice(this.counter,this.counter+3);
+		var nextThree = toArray.slice(this.counter,this.counter+this.incrementBy);
 		_.each(nextThree, function(quote){
 			self.$el.append((new QuoteView({model: quote})).render().$el);
 		});
@@ -25,10 +26,10 @@ var QuotesView = Backbone.View.extend({
 		this.model.models.forEach(function(bBModel, index) {bBModel.set({eyeDee: index+1})})
 	},
 	incrementCounter: function() {
-		if (!(this.counter + 3 > this.model.length-1)) {this.counter += 3};
+		if (!(this.counter + this.incrementBy > this.model.length-1)) {this.counter += this.incrementBy};
 	},
 	decrementCounter: function() {
-		if (!(this.counter - 3 < 0)) {this.counter -= 3};
+		if (!(this.counter - this.incrementBy < 0)) {this.counter -= this.incrementBy};
 	}
 });
 
