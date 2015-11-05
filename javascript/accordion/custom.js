3426de9233ae8f91478291c0ad67823497633c55
Jay Quigley <quigley.james.g@gmail.com>
2015-11-05 16:31:26 -0600

    add some comments to the jQuery code

--- javascript/accordion/custom.js
+++ javascript/accordion/custom.js
@@ -1,14 +1,17 @@
 $(function () {
-	// initial setup: show first
+	// initial setup: show first tab
 	$(".accordion-header div").hide();
 	$(".accordion-header:first-of-type").addClass("active").children("div").show();
 
-	// 
+	// on click of non-active tab, deactivate active tabs & slide-hide all the tabs' child divs . . .
+
 	$(".accordion-header").on("click",function(event) {
 		event.preventDefault();
 		if (!$(this).hasClass("active")) {
 			$(".accordion-header").removeClass("active");
 			$(".accordion-header div").slideUp();
+
+			// . . . then make the clicked-on one active and slide-show its child div
 
 			$(this).addClass("active").children("div").slideToggle();
 		};
