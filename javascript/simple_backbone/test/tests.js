9626aeab6c1a11e6a08b6a8f0f3b3c0c2d73debb
Jay Quigley <quigley.james.g@gmail.com>
2015-11-06 18:14:28 -0600

    add tests for clicks of Next and Previous buttons

--- javascript/simple_backbone/test/tests.js
+++ javascript/simple_backbone/test/tests.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	module("Tests");
 
-	var t = setTimeout(function() {
+	setTimeout(function() {
 		test("There should be three quotes on the page", function() {
 			equal($(".quote").length, 3)
 			ok(true);
 		});	
 	},300);
 
-	var u = setTimeout(function() {
+	setTimeout(function() {
 		clickGames();
 		test("when I use the drop-down menu to select 'games', the word 'movies' is not contained anywhere in any of the `QuoteView`s", function() {
 				var themes = findThemesOnPage();
@@ -43,12 +43,57 @@ $(document).ready(function() {
 		typeWord("");
 	},2000);
 
+	setTimeout(function() {
+		test("Nothing happens when you click the 'previous' button over and over", function() {
+			var originalQuotes = findQuotesOnPage();
+			for (var i = 0; i < 20; i++) {
+				clickPrevious();
+				var quotes = findQuotesOnPage();
+				ok(compareArrays(quotes, originalQuotes));
+			};
+		});
+	},2500);
+
+	setTimeout(function() {
+		test("The quotes change when you click next (if there are more)",function(){
+			var originalQuotes = findQuotesOnPage();
+			clickNext();
+			var quotes = findQuotesOnPage();
+			notOk(compareArrays(originalQuotes,quotes));
+		});
+	},2600);
+
+	setTimeout(function() {
+		test("The quotes do not change when you click next repeatedly (if there are no more)",function(){
+			for (var i = 0; i < 500; i++) {
+				clickNext();
+			}
+			var originalQuotes = findQuotesOnPage();
+			for (var i = 0; i < 20; i++) {
+				clickNext();
+				var quotes = findQuotesOnPage();
+				ok(compareArrays(originalQuotes,quotes));
+			}
+		});
+	},3000);
+
+	setTimeout(function() {
+		typeWord("");
+	},4500);
+
 });
+
 function clickGames() {
 	$(".theme-dropdown").val("games").trigger("click").trigger("change");
 }
 function clickMovies() {
 	$(".theme-dropdown").val("movies").trigger("click").trigger("change");
+}
+function clickPrevious() {
+	$(".previous-button").trigger("click");
+}
+function clickNext() {
+	$(".next-button").trigger("click");
 }
 function typeWord(word) {
 	$(".search").val(word).trigger("input");
@@ -67,3 +112,19 @@ function findQuotesOnPage() {
 	}).toArray();
 	return array;
 }
+function compareArrays(array1,array2){
+	if (array1.length !== array2.length) {	
+		return false
+	}
+	for (var i = 0; i < array1.length; i++) {
+		if (array1[i] !== array2[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+console.log("// tests of compareArrays()")
+console.log("[1,2] equals [1,2]",compareArrays([1,2],[1,2]))
+console.log("[1,2] equals [1,3]",compareArrays([1,2],[1,3]))
+console.log("[1,2] equals [1,2,3]",compareArrays([1,2],[1,2,3]))
